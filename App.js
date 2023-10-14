3857f57b4a76ea671c56f08dbdeae88d077a2ea6
E. Hannah <echannah631@gmail.com>
2023-10-14 00:24:03 -0400

    installed eslint and prettier, searchbar component

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>search</Text>
+          <Searchbar placeholder="Search" />
         </View>
         <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style='auto' />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
@@ -23,11 +24,11 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight,
   },
   search: {
-    backgroundColor: 'green',
+    backgroundColor: "green",
     padding: 16,
   },
   list: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     flex: 1,
     padding: 16,
   },
