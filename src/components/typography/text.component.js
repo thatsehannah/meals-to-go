ad33c957dc0e2263c7087401b679ccab6e93d8d5
E. Hannah <echannah631@gmail.com>
2023-10-21 19:11:10 -0500

    created text component, moved styles from component to own file

---
+++ src/components/typography/text.component.js
@@ -0,0 +1,61 @@
+import styled from "styled-components/native";
+
+//these styles will be applied to all variants (unless they're overwritten)
+const defaultTextStyles = (theme) => `
+  font-family: ${theme.fonts.body};
+  font-weight: ${theme.fontWeights.regular};
+  color: ${theme.colors.text.primary};
+  flex-wrap: wrap;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+const body = (theme) => `
+  font-size: ${theme.fontSizes.body};
+`;
+
+const hint = (theme) => `
+  font-size: ${theme.fontSizes.body};
+`;
+
+const error = (theme) => `
+  color: ${theme.colors.text.error};
+`;
+
+const caption = (theme) => `
+  font-size: ${theme.fontSizes.caption};
+  font-weight: ${theme.fontWeights.bold};
+`;
+
+const label = (theme) => `
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes.body};
+  font-weight: ${theme.fontWeights.medium};
+`;
+
+//the keys are all functions declared above and their values
+//will be used for the corresponding properties
+//would be the same as:
+// const variants = {
+//   body: body,
+//   label: label,
+//   caption: caption,
+//   error: error,
+//   hint: hint
+// };
+const variants = {
+  body,
+  label,
+  caption,
+  error,
+  hint,
+};
+
+export const Text = styled.Text`
+  ${({ theme }) => defaultTextStyles(theme)}
+  ${({ variant, theme }) => variants[variant](theme)}
+`;
+
+Text.defaultProps = {
+  variant: "body",
+};
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,48 +1,20 @@
-import { Text, View, Image } from "react-native";
+import { View, Image } from "react-native";
 import { Card } from "react-native-paper";
-import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import {
+  RestaurantCardCover,
+  Info,
+  Address,
+  Row,
+  Rating,
+  Closed,
+  Icon,
+} from "./restaurant-info-card.styles";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Row = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Closed = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -64,7 +36,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <Card elevation={4}>
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
-          <Title>{name}</Title>
+          <Text>{name}</Text>
           <Row>
             <Rating>
               {ratingArray.map((index) => (
@@ -78,12 +50,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
             )}
             {isClosedTemporarily && (
               <Closed>
-                <Text style={{ color: "red" }}>CLOSED TEMPORARILY</Text>
+                <Text variant="error">CLOSED TEMPORARILY</Text>
                 <Spacer position="left" size="large" />
-                <Image
-                  style={{ width: 15, height: 15 }}
-                  source={{ uri: icon }}
-                />
+                <Icon source={{ uri: icon }} />
               </Closed>
             )}
           </Row>
