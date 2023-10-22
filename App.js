a8e2108f1872ce3ad61790a83e14ce143f56b097
E. Hannah <echannah631@gmail.com>
2023-10-22 12:42:54 -0500

    added screen options option to tab navigator, different from how the docs implemented this

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { ThemeProvider } from "styled-components/native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -11,21 +12,47 @@ import { SafeAreaContainer } from "./src/components/utility/safe-area.component"
 
 const SettingsScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaContainer
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
       <Text>Settings!</Text>
-    </View>
+    </SafeAreaContainer>
   );
 };
 
 const MapScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaContainer
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
       <Text>Map!</Text>
-    </View>
+    </SafeAreaContainer>
   );
 };
 
-export default function App() {
+//object that uses the name of the route defined in Tab.Screen as the key
+//and the icon name from @expo/vector-icons/Ionicons as the value to easily
+//select the icon based on the route in screen options (see below)
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Map: "map",
+  Settings: "settings",
+};
+
+//function that simplifies the process of determining the appropriate icon
+//based on the route given in screen options in Tab.Navigator
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  };
+};
+
+export default App = () => {
   loadFonts();
 
   const Tab = createBottomTabNavigator();
@@ -34,7 +61,7 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator screenOptions={createScreenOptions}>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={MapScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
@@ -44,4 +71,4 @@ export default function App() {
       <ExpoStatusBar style="auto" />
     </>
   );
-}
+};
