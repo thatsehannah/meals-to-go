24bd7650954627ef0bff0aca54ad727be9865672
E. Hannah <echannah631@gmail.com>
2023-10-25 09:38:53 -0500

    added activity indicator to for when things are loading

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator, Colors } from "react-native-paper";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeAreaContainer } from "../../../components/utility/safe-area.component";
@@ -7,9 +7,19 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 import { SearchContainer, RestaurantList } from "./restaurants.screen.styles";
 
 import { RestaurantsContext } from "../../../services/restaurant/restaurants.context";
+import { CenteredView } from "../../../components/utility/centered-view.component";
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+
+  if (isLoading) {
+    return (
+      <CenteredView>
+        <ActivityIndicator size={50} animating={true} color="#0000ff" />
+      </CenteredView>
+    );
+  }
+
   return (
     <>
       <SafeAreaContainer>
