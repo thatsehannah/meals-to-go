946c8c3e485134f623db39245d1a60dccd2fe94c
E. Hannah <echannah631@gmail.com>
2023-10-26 18:15:33 -0500

    moved navigation out of app.js to create navigation stack

--- App.js
+++ App.js
@@ -1,63 +1,14 @@
-import { Text, View } from "react-native";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { ThemeProvider } from "styled-components/native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
 
-import { theme } from "./src/infrastructure/theme";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { loadFonts } from "./src/utils/loadFonts";
-import { SafeAreaContainer } from "./src/components/utility/safe-area.component";
-import { RestaurantsContextProvider } from "./src/services/restaurant/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-
-const SettingsScreen = () => {
-  return (
-    <SafeAreaContainer
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>Settings!</Text>
-    </SafeAreaContainer>
-  );
-};
-
-const MapScreen = () => {
-  return (
-    <SafeAreaContainer
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>Map!</Text>
-    </SafeAreaContainer>
-  );
-};
-
-//object that uses the name of the route defined in Tab.Screen as the key
-//and the icon name from @expo/vector-icons/Ionicons as the value to easily
-//select the icon based on the route in screen options (see below)
-const TAB_ICON = {
-  Restaurants: "restaurant",
-  Map: "map",
-  Settings: "settings",
-};
-
-//function that simplifies the process of determining the appropriate icon
-//based on the route given in screen options in Tab.Navigator
-const createScreenOptions = ({ route }) => {
-  const iconName = TAB_ICON[route.name];
-  return {
-    tabBarIcon: ({ size, color }) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-    tabBarActiveTintColor: "tomato",
-    tabBarInactiveTintColor: "gray",
-  };
-};
+import { theme } from './src/infrastructure/theme';
+import { loadFonts } from './src/utils/loadFonts';
+import { RestaurantsContextProvider } from './src/services/restaurant/restaurants.context';
+import { LocationContextProvider } from './src/services/location/location.context';
+import { Navigation } from './src/infrastructure/navigation';
 
 export default App = () => {
   // loadFonts();
-
-  const Tab = createBottomTabNavigator();
 
   // it makes senses to make the location context provider the top level provider
   // because the restaurants context provider will need the data provided from it
@@ -67,17 +18,11 @@ export default App = () => {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator screenOptions={createScreenOptions}>
-                <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                <Tab.Screen name="Map" component={MapScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
-      <ExpoStatusBar style="auto" />
+      <ExpoStatusBar style='auto' />
     </>
   );
 };
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,76 @@
+import { Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { RestaurantsScreen } from '../../features/restaurants/screens/restaurants.screen';
+import { SafeAreaContainer } from '../../components/utility/safe-area.component';
+
+const SettingsScreen = () => {
+  return (
+    <SafeAreaContainer
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
+      <Text>Settings!</Text>
+    </SafeAreaContainer>
+  );
+};
+
+const MapScreen = () => {
+  return (
+    <SafeAreaContainer
+      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+    >
+      <Text>Map!</Text>
+    </SafeAreaContainer>
+  );
+};
+
+//object that uses the name of the route defined in Tab.Screen as the key
+//and the icon name from @expo/vector-icons/Ionicons as the value to easily
+//select the icon based on the route in screen options (see below)
+const TAB_ICON = {
+  Restaurants: 'restaurant',
+  Map: 'map',
+  Settings: 'settings',
+};
+
+//function that simplifies the process of determining the appropriate icon
+//based on the route given in screen options in Tab.Navigator
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons
+        name={iconName}
+        size={size}
+        color={color}
+      />
+    ),
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  };
+};
+
+export const AppNavigator = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={createScreenOptions}>
+        <Tab.Screen
+          name='Restaurants'
+          component={RestaurantsScreen}
+        />
+        <Tab.Screen
+          name='Map'
+          component={MapScreen}
+        />
+        <Tab.Screen
+          name='Settings'
+          component={SettingsScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
