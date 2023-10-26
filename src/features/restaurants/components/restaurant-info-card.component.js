0d6a61b2a66b51f0af05b127a42c73d371b46842
E. Hannah <echannah631@gmail.com>
2023-10-26 16:13:41 -0500

    made the star key more unique

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,8 +1,8 @@
-import { View, Image } from "react-native";
-import { SvgXml } from "react-native-svg";
+import { View, Image } from 'react-native';
+import { SvgXml } from 'react-native-svg';
 
-import { Spacer } from "../../../components/spacer/spacer.component";
-import { Text } from "../../../components/typography/text.component";
+import { Spacer } from '../../../components/spacer/spacer.component';
+import { Text } from '../../../components/typography/text.component';
 import {
   RestaurantCardCover,
   Info,
@@ -13,21 +13,22 @@ import {
   Icon,
   RestaurantCard,
   IconContainer,
-} from "./restaurant-info-card.styles";
-import star from "../../../../assets/star";
-import open from "../../../../assets/open";
+} from './restaurant-info-card.styles';
+import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Some Restaurant",
-    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    name = 'Some Restaurant',
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+      'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
     ],
-    address = "100 some random street",
+    address = '100 some random street',
     isOpenNow = false,
     rating = 4,
     isClosedTemporarily = true,
+    placeId,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -35,27 +36,42 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <>
       <RestaurantCard elevation={4}>
-        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+        <RestaurantCardCover
+          key={name}
+          source={{ uri: photos[0] }}
+        />
         <Info>
-          <Text variant="label">{name}</Text>
+          <Text variant='label'>{name}</Text>
           <Row>
             <Rating>
               {ratingArray.map((_, index) => (
-                <SvgXml key={index} xml={star} width={20} height={20} />
+                <SvgXml
+                  key={`star-${placeId}-${index}`}
+                  xml={star}
+                  width={20}
+                  height={20}
+                />
               ))}
             </Rating>
             <IconContainer>
               {isOpenNow && (
                 <View>
-                  <SvgXml xml={open} width={25} height={25} />
+                  <SvgXml
+                    xml={open}
+                    width={25}
+                    height={25}
+                  />
                 </View>
               )}
               {isClosedTemporarily && (
                 <Closed>
-                  <Text variant="error">CLOSED TEMPORARILY</Text>
+                  <Text variant='error'>CLOSED TEMPORARILY</Text>
                 </Closed>
               )}
-              <Spacer position="left" size="large" />
+              <Spacer
+                position='left'
+                size='large'
+              />
               <Icon source={{ uri: icon }} />
             </IconContainer>
           </Row>
