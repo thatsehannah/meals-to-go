50ac203a64106cfd07b760265da6b12195b5d31e
E. Hannah <echannah631@gmail.com>
2023-11-06 12:36:02 -0600

    created favorites bar, able to navigate to restaurant detail from favorites bar

--- src/components/favorites/favorites-bar.component.js
+++ src/components/favorites/favorites-bar.component.js
@@ -1,5 +1,47 @@
-import { Text } from 'react-native';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
+import styled from 'styled-components/native';
+import { CompactRestaurantInfo } from '../restaurant/compact-restaurant-info.component';
+import { Spacer } from '../spacer/spacer.component';
+import { Text } from '../typography/text.component';
+import { Fragment } from 'react';
 
-export const FavoritesBar = () => {
-  return <Text>Favorites Bar</Text>;
+const FavoritesContainer = styled.View`
+  padding: 10px;
+`;
+
+export const FavoritesBar = ({ favorites, onNavigate }) => {
+  if (!favorites.length) {
+    return null;
+  }
+
+  return (
+    <FavoritesContainer>
+      <Text variant='caption'>Favorites</Text>
+      <Spacer
+        position='left'
+        size='large'
+      />
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {favorites.map((restaurant) => {
+          const key = restaurant.name.split(' ').join('');
+          return (
+            <Fragment key={key}>
+              <TouchableOpacity
+                onPress={() => onNavigate('RestaurantDetail', restaurant)}
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+              <Spacer
+                position='left'
+                size='large'
+              />
+            </Fragment>
+          );
+        })}
+      </ScrollView>
+    </FavoritesContainer>
+  );
 };
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -8,6 +8,7 @@ import { Spacer } from '../../../components/spacer/spacer.component';
 import { RestaurantList } from './restaurants.screen.styles';
 
 import { RestaurantsContext } from '../../../services/restaurant/restaurants.context';
+import { FavoritesContext } from '../../../services/favorites/favorites.context';
 import { CenteredView } from '../../../components/utility/centered-view.component';
 import { Search } from '../components/search.component';
 import { FavoritesBar } from '../../../components/favorites/favorites-bar.component';
@@ -15,6 +16,7 @@ import { FavoritesBar } from '../../../components/favorites/favorites-bar.compon
 export const RestaurantsScreen = ({ navigation }) => {
   const { isRestaurantsLoading, error, restaurants } =
     useContext(RestaurantsContext);
+  const { favorites } = useContext(FavoritesContext);
   const [isToggled, setIsToggled] = useState(false);
 
   if (isRestaurantsLoading) {
@@ -36,7 +38,12 @@ export const RestaurantsScreen = ({ navigation }) => {
           isFavoritesToggled={isToggled}
           onFavoritesToggle={() => setIsToggled(!isToggled)}
         />
-        {isToggled && <FavoritesBar />}
+        {isToggled && (
+          <FavoritesBar
+            favorites={favorites}
+            onNavigate={navigation.navigate}
+          />
+        )}
         <RestaurantList
           data={restaurants}
           renderItem={({ item }) => {
